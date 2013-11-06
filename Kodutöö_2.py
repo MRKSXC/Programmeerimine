01252105f083dfb34553be4ad5d01bc4a1294cd9
Markus Land <Markus.lnd@gmail.com>
2013-11-06 10:53:36 +0200

    Parandus

--- Kodutöö_2.py
+++ Kodutöö_2.py
@@ -2,7 +2,6 @@
 # coding: latin-1
 
 Visiitkaart = raw_input('Sisesta visiitkaardi nimi :')
-Ettev6te = raw_input('Sisesta ettevõtte nimi :')
 Nimi = raw_input('Sisesta oma nimi :')
 Amet = raw_input('Sisesta oma amet :')
 Kontakt = raw_input('Sisesta Telefoni number :')
@@ -13,7 +12,6 @@ Joon = '-'
 print '<', Joon.center(60,'-'), '>'
 print '/', Tyhjus.center(60,' '), '\ '
 print '|', Visiitkaart.center(60,' '),  '|'
-print '|', Ettev6te.center(60,' '), '|'
 print '|', Tyhjus.center(60,' '), '|'
 print '|', Nimi.center(60,' '), '|'
 print '|', Amet.center(60,' '), '|'
