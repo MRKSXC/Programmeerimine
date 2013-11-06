37d08744f9693d142ce605eb20653442e8221c26
Markus Land <Markus.lnd@gmail.com>
2013-11-06 09:58:51 +0200

    Parandus

--- Kodutöö_2.py
+++ Kodutöö_2.py
@@ -10,14 +10,14 @@ Kontakt = raw_input('Sisesta Telefoni number :')
 Tyhjus = ' '
 Joon = '-'
 
-print '<', Joon.center(60,'-'), 	'>'
-print '/', Tyhjus.center(60,' '), 	'\ '
-print '|', Visiitkaart.center(60,' '),	'|'
+print '<', Joon.center(60,'-'), '>'
+print '/', Tyhjus.center(60,' '), '\ '
+print '|', Visiitkaart.center(60,' '),  '|'
 print '|', Ettev6te.center(60,' '),	'|'
-print '|', Tyhjus.center(60,' '),	'|'
-print '|', Nimi.center(60,' '),		'|'
-print '|', Amet.center(60,' '),		'|'
-print '/', Kontakt.center(60,' '), '\ '
-print '>', Joon.center(60,'-'),		'<'
+print '|', Tyhjus.center(60,' '),	  '|'
+print '|', Nimi.center(60,' '),		  '|'
+print '|', Amet.center(60,' '),		  '|'
+print '/', Kontakt.center(60,' '),  '\ '
+print '>', Joon.center(60,'-'),		  '<'
 
 
