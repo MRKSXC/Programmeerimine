1526853c4c500036b83ab7892486d2e741a85b6c
Markus Land <Markus.lnd@gmail.com>
2013-12-04 13:57:28 +0200

    tunnitööd

---
+++ Tunnitööd.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# coding: latin-1
+import os
+import random
+clear = os.system('clear');
+
+mini = int(raw_input('Sisesta esimene number :'))
+maxx = int(raw_input('Sisesta teine number :'))
+N = int(raw_input('Mitut arvu soovite :'))
+i = 0
+
+while i < N:
+	i = i + 1
+	print random.randint(mini, maxx)
+	
+	
+	#!/usr/bin/python
+# coding: latin-1
+import os
+import random
+clear = os.system('clear');
+
+a = 0
+b = '#'
+c = '*'
+d = 20
+
+
+
+
+while a <= d:
+	print b
+	b = b + '#'
+	a = a + 1
+
+	
+	
+	
+	#!/usr/bin/python
+# coding: latin-1
+
+import os
+import random
+clear = os.system('clear');
+
+mini = int(raw_input('Sisesta esimene number :'))
+maxx = int(raw_input('Sisesta teine number :'))
+N = int(raw_input('Mitut arvu soovite :'))
+i = 0
+summa = 0
+
+while i < N:
+	i = i + 1
+	print random.randint(mini, maxx)
+	summa = summa + random.randint(mini, maxx)
+
+
+keskmine = float(summa)/N
+print 'Praktiline keskmine={:.2f}'.format(keskmine)
+
+
+
+'''' 
+a = 1
+b = 1
+c = 5
+
+while n <= c:
+	b = b*n
+	n = n+1
+
+
+
+	
+	
+	
+	
+	
